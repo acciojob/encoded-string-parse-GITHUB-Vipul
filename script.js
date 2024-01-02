e36dd62ec5296169f6f473648bfdfa41db400880
GITHUB-Vipul <126069867+GITHUB-Vipul@users.noreply.github.com>
2024-01-02 19:14:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 const parseCode = (str) => {
   // your code here
-	let s=str.split("000");
+	let arr=[];
+	let j=0;
+	let s="";
+	for(let i=0;i<str.length;i++)
+		{
+			if(str.at(i)=='0')
+			{
+				
+				if(s!="")
+				{
+					arr[j]=s;
+					j++;
+				}
+				s="";
+			}
+			else
+			s+=str.at(i);
+		}
+	arr[j]=s;
 	let obj={
 	};
-obj.firstName=`${s[0]}`;
-	obj.lastName=`${s[1]}`;
-	obj.id=`${s[2]}`;
+obj.firstName=`${arr[0]}`;
+	obj.lastName=`${arr[1]}`;
+	obj.id=`${arr[2]}`;
 	return obj;
 	
 };
