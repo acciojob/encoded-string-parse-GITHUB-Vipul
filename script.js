2cb25fd1e4137d809e992083f056f2ee4b284c0c
GITHUB-Vipul <126069867+GITHUB-Vipul@users.noreply.github.com>
2024-01-02 18:54:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const parseCode = (str) => {
   // your code here
+	let s=str.split("000");
+	let obj={
+	};
+obj.firstName=`${s[0]}`;
+	obj.lastName=`${s[1]}`;
+	obj.id=`${s[2]}`;
+	return obj;
+	
 };
 
 // Do not change the code below
-const str = prompt("Enter str: ");
+const str =  prompt("Enter str: ");
 alert(JSON.stringify(parseCode(str)));
